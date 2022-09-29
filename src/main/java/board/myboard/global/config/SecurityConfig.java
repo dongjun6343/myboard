6d05dcf219f59098a2e847888ec50f5822cd1d70
dongjun6343 <dongjun6343@naver.com>
2022-09-29 14:15:44 +0900

    회원수정 테스트 케이스 추가

---
+++ src/main/java/board/myboard/global/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package board.myboard.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+import javax.servlet.Filter;
+
+/**
+ * 시큐리티를 이용한 JSON 데이터로 로그인
+ *
+ * 스프링 버전이 업데이트 됨에 따라 WebSecurityConfigurerAdapter와 그 외 몇 가지들이 Deprecated.
+ *
+ * 기존에는 WebSecurityConfigurerAdapter를 상속받아 설정을 오버라이딩 하는 방식이었는데
+ * 바뀐 방식에서는 상속받아 오버라이딩하지 않고 모두 Bean으로 등록을 합니다.
+ * (바뀐 방식 공부 후 적용)
+ */
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfiguration {
+
+}
